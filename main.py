83ec26849e377cc373f668e88efbafe0efca0dba
Octavio Araujo Rosales <oaraujor_@hotmail.com>
2025-06-12 22:49:03 -0600

    added input boxes/bug: overlapping txt bxs

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import ctypes
 
 from custLinAlg import *
 
-ratio = .70
-angle1 = 90
+ratio = .66
+angle1 = 45
 angle2 = 90
 max_generations = 9
 
@@ -43,15 +43,21 @@ def main():
     #initialize pygame
     pygame.init()
     screen = pygame.display.set_mode((screen_width,screen_height), pygame.RESIZABLE)
-    title_fnt = pygame.font.SysFont('consolas', 56)
-    frstAngle_fnt = pygame.font.Font(None, 40)
-    secAngle_fnt = pygame.font.Font(None, 40)
-    usr_frstAngle_txt = 'First Angle'
-    usr_secAngle_txt = 'Second Angle'
     pygame.display.set_caption("Fractal Canopy")
-    os.environ['SDL_VIDEO_CENTERED'] = '1'
     clock = pygame.time.Clock()
-    run = True
+    title_fnt = pygame.font.SysFont('consolas', 56)
+    os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+    usr_frstAngle_txt = '30'
+    frstAngle_fnt = pygame.font.SysFont('consolas', 40)
+    frstAngle_rect = pygame.Rect(x_padding, 20, 60, 40)
+    is_activeFrstAngle = False
+
+    usr_secAngle_txt = '60'
+    secAngle_fnt = pygame.font.SysFont('consolas', 40)
+    secAngle_rect = pygame.Rect(x_padding, 80, 60, 40)
+    is_activeSecAngle = False
+
 
     x = screen_width / 2    #start in the middle
     y = (screen_height) /2
@@ -62,24 +68,63 @@ def main():
     end = [x, y]
     n = 1
 
+    run = True
+
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE: #close if ESC is pressed
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE: #close if ESC is pressed
                     run = False
-            #if event.type == pygame.KEYDOWN
+                if is_activeFrstAngle:
+                    if event.key == pygame.K_BACKSPACE:
+                        usr_frstAngle_txt = usr_frstAngle_txt[:-1]
+                    else:
+                        usr_frstAngle_txt += event.unicode
+                if is_activeSecAngle:
+                    if event.key == pygame.K_BACKSPACE:
+                        usr_secAngle_txt = usr_secAngle_txt[:-1]
+                    else:
+                        usr_secAngle_txt += event.unicode
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if frstAngle_rect.collidepoint(event.pos):
+                    is_activeFrstAngle = True
+                else:
+                    is_activeFrstAngle = False
+                if secAngle_rect.collidepoint(event.pos):
+                    is_activeSecAngle = True
+                else:
+                    is_activeSecAngle = False
 
         screen.fill("black")
         #render section
+
+        if is_activeFrstAngle:
+            color_FrstAngle = pygame.Color(255,255,255)
+        else:
+            color_FrstAngle = pygame.Color(57,255,20)
+
+        if is_activeSecAngle:
+            color_SecAngle = pygame.Color(255,255,255)
+        else:
+            color_SecAngle = pygame.Color(57,255,20)
+
+        pygame.draw.rect(screen, color_FrstAngle, frstAngle_rect, 4) #draw first anglebox
+        pygame.draw.rect(screen, color_SecAngle, secAngle_rect, 4) #draw second anglebox
 
         rendered_title = title_fnt.render("Fractal Canopy", True, (255,255,255))
         rendered_frstAngle = frstAngle_fnt.render(usr_frstAngle_txt, True, (255,255,255))
         rendered_secAngle = secAngle_fnt.render(usr_secAngle_txt, True, (255,255,255))
 
         screen.blit(rendered_title, (x_padding, 10))
-        screen.blit(rendered_frstAngle, (x_padding, screen_height / 2))
-        screen.blit(rendered_secAngle, (x_padding, screen_height / 1.7))
+        screen.blit(rendered_frstAngle, (frstAngle_rect.x + 5, frstAngle_rect.y + 5))
+        screen.blit(rendered_secAngle, (secAngle_rect.x + 5, secAngle_rect.y + 5))
+        
+        frstAngle_rect.w = max(50, rendered_frstAngle.get_width() + 10)
+        secAngle_rect.w = max(50, rendered_secAngle.get_width() + 10)
+
 
         pygame.draw.line(screen, (57,255,20), start, end, 1)
         initial_direction = [0, -(screen_height / 4)] # Vector pointing upward
