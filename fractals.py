877af5bd1c2300c5ea0fef62c97159f49fd6ba4c
Octavio Araujo Rosales <oaraujor_@hotmail.com>
2025-07-09 00:47:55 -0600

    rfctr/implemented methods for textbox class

--- fractals.py
+++ fractals.py
@@ -6,15 +6,36 @@ class TextBox:
         self.font = font
         self.text = default_text
         self.active = False
+        self.color_active = pygame.Color(57, 255, 20)
+        self.color_inactive = pygame.Color(255, 255, 255)
+        self.color = self.color_inactive
+
 
     def handle_event(self, event):
-        pass
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.active = True
+            else:
+                self.active = False
+            self.color = self.color_inactive if self.active else self.color_inactive
+
+        elif event.type == pygame.KEYDOWN and self.active:
+            if event.key == pygame.K_BACKSPACE:
+                self.text = self.text[:-1]
+            elif event.unicode.isnumeric() or event.unicode in ['.', '-']:
+                self.text += event.unicode
 
     def draw(self, screen):
-        pass
+        pygame.draw.rect(screen, self.color, self.rect, 4)
+        rendered_text = self.font.render(self.text, True, self.color)
+        screen.blit(rendered_text, (self.rect.x + 5, self.rect.y + 5))
+        self.rect.w = max(50, rendered_text.get_width() + 10)
 
-    def get_value(self):
-        pass
+    def get_value(self, default = 30):
+        try:
+            return float(self.text)
+        except ValueError:
+            return default
 
 class FractalTree:
     def __init__(self, screen_size, start, end, max_gen = 8, angle1 = 30, angle2 = 30, ratio = 0.6):
